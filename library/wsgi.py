96314478743ca36c8ee51090861ba28c254e2e4a
anas srikou <robin208@hotmail.fr>
2016-11-14 13:08:39 +0100

    updated wsgi for Cling

--- library/wsgi.py
+++ library/wsgi.py
@@ -10,9 +10,10 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-# from dj_static import Cling
+from dj_static import Cling
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library.settings")
 
-# application = Cling(get_wsgi_application())
+application = Cling(get_wsgi_application())
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
