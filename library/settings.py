6a0fd7b5eda18912cff623f0ea85826c3e8755fe
rev <robin208@hotmail.fr>
2016-11-14 14:31:57 +0100

    Update settings.py

--- library/settings.py
+++ library/settings.py
@@ -166,3 +166,48 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 LOGIN_URL = '/login/'
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+
+# LOGGING
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': False,
+	'filters': {
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse'
+		}
+	},
+	'handlers': {
+		'mail_admins': {
+			'level': 'ERROR',
+			'filters': ['require_debug_false'],
+			'class': 'django.utils.log.AdminEmailHandler'
+		}
+	},
+	'loggers': {
+		'django.request': {
+			'handlers': ['mail_admins'],
+			'level': 'ERROR',
+			'propagate': True,
+		},
+	}
+}
+
+# Admins
+ADMINS = (
+	('REV', 'anas.srikou@gmail.com),
+)
+
+if DJANGO_MODE == 'production':
+	EMAIL_HOST = 'smtp.sendgrid.net',
+	EMAIL_HOST_USER = os.getenv('SENDGRID_USERNAME')
+	EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_PASSWORD')
+	EMAIL_PORT = 587
+	EMAIL_USE_TLS = True
